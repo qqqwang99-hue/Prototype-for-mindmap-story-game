abd8cd8b6b44c01d3af6e636961ea5beeb2eecd1
qqqwang99-hue <qqqwang99@gmail.com>
2025-10-08 18:10:24 -0600

    Add files via upload
    Added first prototype files

---
+++ Fantasy World Building (possible college admission project if polished enough).py
@@ -0,0 +1,19 @@
+#Prototype 10/7/2025
+import math, time, random
+
+welcome = '''Welcome to the Fantasy World Building Game, where you will explore themes of broken trust, healing, and
+learning to love yourself for who you are instead of what other people say you are. In this game, you will be roleplaying as
+a character in a fantasy world where your choices and outcomes are connected on a deeper level.'''
+print(welcome)
+name = input("Enter the name of your character: ")
+print(f"Hello {name}.")
+trustinself_score = 0
+trustinother_score = 0
+
+
+
+
+
+
+
+
